eb167927bd2a92da0f07139943d4453d59ba6b9b
Bernardo Augusto <bernardo.augusto.bastos@gmail.com>
2020-05-16 15:26:10 -0300

    interface

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import User from '../schemas/User'
+
+class UserController {
+  public async index (req: Request, res: Response): Promise<Response> {
+    const users = await User.find()
+
+    return res.json(users)
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const user = await User.create(req.body)
+
+    const fullName: String = user.fullName()
+    console.log(fullName)
+    return res.json(user)
+  }
+}
+
+export default new UserController()
